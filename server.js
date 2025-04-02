bc5055d2359c75fab4bb00cae48fc4aafdbc2482
iranhonorato <antonioihsj@al.insper.edu.br>
2025-04-02 18:26:56 -0300

    Inicializando API de gerenciamento de alimentos

---
+++ server.js
@@ -0,0 +1,43 @@
+import Fastify from 'fastify'
+
+const fastify = Fastify({
+    logger: true
+})
+
+
+
+
+// Lista alimentos
+fastify.get('/alimentos', async (request, reply) => {
+})
+
+
+
+// Adicona alimentos
+fastify.post('/alimentos', async (request, reply) => {
+})
+
+
+
+// Edita aliemntos
+fastify.put('/alimentos/:id', async (request, reply) => {
+})
+
+
+
+
+
+// Deleta alimentos
+fastify.delete('/alimentos/:id', async (request, reply) => {
+})
+
+
+
+
+
+fastify.listen({ port: 777 }, async (err, address) => {
+    if (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+})
